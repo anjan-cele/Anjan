5c21149a5a9285d56f5c7620cba44e31eb3cd9b3
anjan-cele <anjanbca@gmail.com>
2019-08-24 16:49:42 +0530

    more changes
    more changes

--- ScaffoldingSample/ScaffoldingSample/Controllers/HomeController.cs
+++ ScaffoldingSample/ScaffoldingSample/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,9 +13,57 @@ namespace ScaffoldingSample.Controllers
         {
             ViewBag.Title = "Home Page";
 
-            RedirectToRoute("Employee", new { controller = "Employee", action = "Index" });
+            //RedirectToRoute("Employee", new { controller = "Employee", action = "Index" });
 
-            return View();
+            //IsMobileDevice();
+
+
+            if (Request.UserAgent.Contains("Mobi") == true)
+            {
+                return View("MobileIndex");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public bool IsMobileDevice()
+        {
+            bool ret = Request.Browser.IsMobileDevice;
+            string userAgent = Request.UserAgent;
+
+            if (!ret)
+            {
+                // Use regular expression
+                Regex b = new Regex(@"(android|bb\d+|meego)
+                                    .+mobile|avantgo|bada\/|blackberry|
+                                    blazer|compal|elaine|fennec|hiptop|
+                                    iemobile|ip(hone|od)|iris|kindle|lge
+                                    |maemo|midp|mmp|mobile.+firefox|
+                                    netfront|opera m(ob|in)i|palm(os)
+                                    ?|phone|p(ixi|re)\/|plucker|
+                                    pocket|psp|series(4|6)0|symbian|
+                                    treo|up\.(browser|link)|vodafone|
+                                    wap|windows ce|xda|xiino",
+                  RegexOptions.IgnoreCase |
+                  RegexOptions.Multiline);
+
+                ret = b.IsMatch(userAgent);
+
+                // Check user agent for certain words
+                ret = !ret
+                        ? userAgent.ToLower().Contains("iphone")
+                        : true;
+                ret = !ret
+                        ? userAgent.ToLower().Contains("android")
+                        : true;
+                // etc.
+            }
+            return ret;
         }
     }
+            
+  
 }
